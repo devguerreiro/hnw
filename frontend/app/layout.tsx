a311097915fbcff06515f02cb484b9491ad41873
Luis Guerreiro <devguerreiro@icloud.com>
2026-02-17 15:20:13 -0300

    refactor(frontend): :heavy_minus_sign: remove sonner

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Toaster } from "sonner";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,7 +28,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster richColors />
       </body>
     </html>
   );
